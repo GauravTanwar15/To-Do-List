9f11ff8cc3e8ea2f3cc02460fe44675b76d18a28
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-04-01 20:49:04 +0530

    added EJS

--- app.js
+++ app.js
@@ -2,44 +2,37 @@
 
 const express = require("express");
 const bodyparser = require("body-parser");
-
+//using express
 const app = express();
-
+//declaring array
+var items = ["buy food", "cook food", "make food"];
+//using EJS
 app.set('view engine', 'ejs');
+//using body parser
+app.use(bodyparser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
-    varday=" ";
+    //javascript objects
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
 
-switch (currentDay) {
-    case 0:
-        day = "sunday";
-        break;
-    case 1:
-         day = "Monday";
-         break;
-     case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-     case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "friday";
-        break;
-    case 6:
-        day = "saturday";
-        break;
-    default:
-    console.log("error!" + currentDay);
+    var day = today.toLocaleDateString("en-US", options);
       
-}
-res.render("list", {kindofDay: day});
+res.render("list", {kindofDay: day, newListItems: items});
 });
+
+
+app.post("/", function(req, res){
+var item = req.body.newItem;
+
+items.push(item);
+
+   res.redirect("/");
+})
 
 app.listen(3000, function(){
     console.log("server started on port 3000");
